394bcce66133c3e3c441e25149dddc377baedecc
Cole Wilkison <wilkison.cole@gmail.com>
2020-12-10 10:24:03 -0500

    Revert "fixed font, working on detail page now"
    This reverts commit fbc3631d44ceb240a62df38b04dc96e7a0ed50c0.

--- cardinal/src/App.vue
+++ cardinal/src/App.vue
@@ -73,10 +73,8 @@ export default {
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap");
-
 #app {
-  font-family: "Montserrat", sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
